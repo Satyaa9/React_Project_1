5cb9f22eb2c2c35c87aee675a8c8f321c6fe07ba
Satish <dadassatish6@gmail.com>
2025-12-10 23:28:14 +0530

    new

--- src/Components/TrendingRecipe.jsx
+++ src/Components/TrendingRecipe.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
+import React from "react";
+import Slider from "react-slick";
+import useFetch from "./useFetch";
+import RecipeCard from "./RecipeCard";
+import { Clock, Loader } from "lucide-react";
 
-function TrendingRecipe() {
+const TrendingRecipe = ({ title, fetchUrl }) => {
+  const { data, loading, error } = useFetch(fetchUrl);
+  console.log("my meal :", data?.meals);
+
+  const meals = data?.meals || [];
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    arrows: false,
+    speed: 600,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+  };
+
+  if (loading)
+    return (
+      <div className="text-center p-8 text-gray-300">
+        <Loader className="animate-spin inline-block mr-2 text-blue-400" />
+        Loading {title}...
+      </div>
+    );
+
   return (
-    <div>
-      Trending recipe
-    </div>
-  )
-}
+    <>
+      <section className="mt-2 mx-auto">
+        <h2
+          className="text-3xl font-extrabold text-gray-100 mb-6 tracking-tight
+   border-1-4 border-y-amber-400 pl-4 flex items-center"
+        >
+          <Clock className="w-6 h-6 mr-3 text-blue-500" />
+          {title}
+        </h2>
 
-export default TrendingRecipe
+        <div className="w-full mx-auto">
+          <Slider {...settings}>
+            {meals.map((meal) => (
+              <div key={meal.idMeal} className="px-10 flex justify-around">
+                <div
+                  className="relative bg-gray-900 rounded-xl shadow-black/50 overflow-hidded
+                   group transform transition duration-500 cursor-pointer border border-gray-800
+                   hover:shadow-blue-600/50 mb-5"
+                >
+                  <div
+                    className="absolute inset-0 rounded-xl border-2 border-transparent
+                   group-hover:border-blue-500/80 transition duration-500"
+                  ></div>
+
+                  <div className="flex justify-center items-center p-5">
+                    <img
+                      src={meal?.strMealThumb}
+                      alt=""
+                      className="h-[120px] w-[120px] rounded-xl border border-yellow-400 transition duration-500
+                      group-hover:scale-105"
+                    />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default TrendingRecipe;
